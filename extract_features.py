a16a1838fb2e4c11106df1b91f04bb166068ab2f
ChriDiDi <christian.didiego@mail.polimi.it>
2023-11-24 23:00:16 +0100

    added features extractor; first model trial (accuracy on 128 = 0.89)

--- extract_features.py
+++ extract_features.py
@@ -10,9 +10,20 @@ Amplitude features
 - RD: D1/D2
 - AMSP: amplitude max slope point (massimo derivata prima)
 - RN: ADN/ASP
+
+Time-related feature
+- TP: time interval from DP to SP
+- T1: time interval from DN to SP
+- T2: time interval from DP to DN
+- pi2pi_pr: time interval from SPi-1 to SPi
+- pi2pi_po time interval from SPi to SPi+1
 '''
 
-def extract_features(df_features, filtered_ppg):
+import numpy as np
+
+def extract_features(df_features, filtered_ppg, fs):
+
+    ##Amplitude features
     peak_pos = df_features['peak_pos']
     mask_onset = df_features['onset'].notnull()
     mask_notch = df_features['diastolic_notch'].notnull()
@@ -27,5 +38,33 @@ def extract_features(df_features, filtered_ppg):
     df_features['rd'] = df_features['d1'] / df_features['d2']
     # AMSP TODO
     df_features['rn'] = df_features['adn'] / df_features['asp']
+
+    ##Time-related features
+    df_features['T1'] = df_features['diastolic_notch']/128 - df_features['peak_pos']/128
+    df_features['T2'] = df_features['diastolic_notch']/128 - df_features['onset']/128
+
+
+    # defining pi2pi_pr: time interval from SPi-1 to SPi
+    pi2pi_pr = []
+    # Add a first 0 value to align the dimension of the array
+    pi2pi_pr.append(0)
+    for i in range(1, len(df_features)):
+        val = df_features.loc[i]['peak_pos'] / fs - df_features.loc[i - 1]['peak_pos'] / fs
+        pi2pi_pr.append(val)
+
+    pi2pi_pr = np.array(pi2pi_pr)
+    df_features['pi2pi_pr'] = pi2pi_pr
+
+
+    # pi2pi_po time interval from SPi to SPi+1
+    pi2pi_po = []
+    for i in range(len(df_features) - 1):
+        val = df_features.loc[i + 1]['peak_pos'] / fs - df_features.loc[i]['peak_pos']/ fs
+        pi2pi_po.append(val)
+
+    # Add a last None value to align the dimension of the array
+    pi2pi_po.append(None)
+    pi2pi_po = np.array(pi2pi_po)
+    df_features['pi2pi_po'] = pi2pi_po
 
     return df_features
